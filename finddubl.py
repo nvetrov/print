45b8a9e3f21435c690d7a641739f20034660050e
Nikolai Vetrov <nvetrov@gmail.com>
2019-12-20 00:04:00 +0300

    done

--- finddubl.py
+++ finddubl.py
@@ -1,2 +1,15 @@
-from pandas import read_csv
+from collections import Counter
 
+with open('Printers.csv', 'r') as file:
+    ff = file.read().split()
+dict_printer = {'User': []}
+for i in range(len(ff) - 1):
+    if ff[i].startswith('S'):
+        dict_printer['User'].append(ff[i].split('|')[0])
+counter = Counter(dict_printer['User'])
+for computer in counter:
+    if counter[computer] > 1:
+         print(computer)
+    else:
+        continue
+print('Done')
